9c1fbf31c1a8b0ab189e579d46c623fa29883132
Hannah Kim <devhkim13@gmail.com>
2023-10-25 15:39:03 -0700

    Create tests for the Gameboard factory

---
+++ src/scripts/tests/gameboard.test.js
@@ -0,0 +1,131 @@
+import Gameboard from '../gameboard';
+import Ship from '../ship';
+
+test('place a ship of length 2 (horizontally) in the top left corner', () => {
+  const gameboard = Gameboard();
+  const ship1 = Ship(2);
+  gameboard.placeShip(ship1, ['1', 'a']);
+  expect(gameboard.getState()).toEqual([
+    [ship1, ship1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('place a ship of length 2 (horizontally) in the top right corner', () => {
+  const gameboard = Gameboard();
+  const ship1 = Ship(2);
+  gameboard.placeShip(ship1, ['1', 'j']);
+  expect(gameboard.getState()).toEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('place a ship of length 2 (vertically) in the bottom right corner', () => {
+  const gameboard = Gameboard();
+  const ship1 = Ship(2);
+  gameboard.placeShip(ship1, ['10', 'j']);
+  expect(gameboard.getState()).toEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('place a ship on top of another ship', () => {
+  const gameboard = Gameboard();
+  const ship1 = Ship(2);
+  gameboard.placeShip(ship1, ['5', 'e']);
+  const ship2 = Ship(4);
+  gameboard.placeShip(ship2, ['5', 'f']);
+  expect(gameboard.getState()).toEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, ship1, ship1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('place an attack that hits a ship', () => {
+  const gameboard = Gameboard();
+  const ship1 = Ship(2);
+  gameboard.placeShip(ship1, ['1', 'a']);
+  gameboard.receiveAttack(['1', 'b']);
+  expect(gameboard.getState()).toEqual([
+    [ship1, 'hit', 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('place an attack that misses a ship', () => {
+  const gameboard = Gameboard();
+  const ship1 = Ship(2);
+  gameboard.placeShip(ship1, ['1', 'a']);
+  gameboard.receiveAttack(['4', 'b']);
+  expect(gameboard.getState()).toEqual([
+    [ship1, ship1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 'miss', 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('check if all ships are sunk', () => {
+  const gameboard = Gameboard();
+  const ship1 = Ship(2);
+  gameboard.placeShip(ship1, ['1', 'a']);
+  gameboard.receiveAttack(['1', 'a']);
+  gameboard.receiveAttack(['1', 'b']);
+  expect(gameboard.isAllSunk()).toEqual(true);
+});
+
+test('check if all ships are sunk', () => {
+  const gameboard = Gameboard();
+  const ship1 = Ship(2);
+  gameboard.placeShip(ship1, ['1', 'a']);
+  gameboard.receiveAttack(['1', 'a']);
+  expect(gameboard.isAllSunk()).toEqual(false);
+});
