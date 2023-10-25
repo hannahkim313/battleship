68ec17bc82c067efdb2310656571f869c6b96cfd
Hannah Kim <devhkim13@gmail.com>
2023-10-24 17:02:54 -0700

    Create tests for the Ship factory

---
+++ src/scripts/tests/ship.test.js
@@ -0,0 +1,15 @@
+import Ship from '../ship';
+
+test('check if ship is sunk after hitting less than total length', () => {
+  const ship = Ship(3);
+  ship.hit();
+  expect(ship.isSunk()).toEqual(false);
+});
+
+test('check if ship is sunk after hitting total length', () => {
+  const ship = Ship(3);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toEqual(true);
+});
