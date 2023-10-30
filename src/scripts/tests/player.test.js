5deef45eebd0c9061dd76939da0eaf6c61e28a21
Hannah Kim <devhkim13@gmail.com>
2023-10-30 15:51:29 -0700

    Create tests for the Player factory

---
+++ src/scripts/tests/player.test.js
@@ -0,0 +1,94 @@
+import Player from '../player';
+import Gameboard from '../gameboard';
+import Ship from '../ship';
+
+const userGameboard = Gameboard();
+const userShip = Ship(2);
+userGameboard.placeShip(userShip, ['1', 'a']);
+const computerGameboard = Gameboard();
+const computerShip = Ship(2);
+computerGameboard.placeShip(computerShip, ['5', 'd']);
+const user = Player();
+user.attack(computerGameboard, ['1', 'a']);
+user.attack(computerGameboard, ['5', 'd']);
+
+test('place an attack on a missed coordinate', () => {
+  expect(user.attack(computerGameboard, ['1', 'a'])).toEqual(null);
+  expect(computerGameboard.getState()).toEqual([
+    ['miss', 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 'hit', computerShip, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('place an attack on an already hit coordinate', () => {
+  expect(user.attack(computerGameboard, ['5', 'd'])).toEqual(null);
+  expect(computerGameboard.getState()).toEqual([
+    ['miss', 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 'hit', computerShip, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('place an attack on an empty coordinate', () => {
+  expect(user.attack(computerGameboard, ['5', 'f'])).toEqual(null);
+  expect(computerGameboard.getState()).toEqual([
+    ['miss', 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 'hit', computerShip, 'miss', 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('place an attack on a ship', () => {
+  user.attack(computerGameboard, ['5', 'e']);
+  expect(computerGameboard.getState()).toEqual([
+    ['miss', 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 'hit', 'hit', 'miss', 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
+
+test('place an attack made by the computer', () => {
+  const computer = Player();
+  computer.attack(userGameboard);
+  expect(userGameboard.getState()).not.toBe([
+    [userShip, userShip, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+});
