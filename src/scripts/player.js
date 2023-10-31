7a023d06d11b4e4aa795b482ef734893f329d3d9
Hannah Kim <devhkim13@gmail.com>
2023-10-30 20:04:23 -0700

    Implement the ability to place an attack on a random coordinate of the board

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,5 +1,18 @@
 const Player = () => {
-  const attack = (board, coords) => board.receiveAttack(coords);
+  const generateCoords = () => {
+    const getRandomNum = () => Math.floor(Math.random() * 9);
+    const letters = 'abcdefghij';
+
+    return [`${getRandomNum()}`, letters[getRandomNum()]];
+  };
+
+  const attack = (board, coords = null) => {
+    if (coords === null) {
+      return board.receiveAttack(generateCoords());
+    }
+
+    return board.receiveAttack(coords);
+  };
 
   return {
     attack,
